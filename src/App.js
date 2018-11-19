03bf6204d7419462a5dc9c9aa4d81d414402b05b
Monica Tamaru Jones <mntamaru@gmail.com>
2018-11-19 18:08:07 +0000

    automatically backed up by learn

--- src/App.js
+++ src/App.js
@@ -27,10 +27,10 @@ const mapStateToProps = (state) => {
   };
 };
 
-const mapDispatchToProps = dispatch => {
-  return {
-    addItem: () => {dispatch(addItem())};
-  };
-};
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addItem: () => {dispatch(addItem())};
+//   };
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, { addItem })(App);
